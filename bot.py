f56081a833d5d3b6c671a9e9216b8e65886df6e3
mfazin <78682897+mfazin@users.noreply.github.com>
2021-02-17 10:54:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -115,7 +115,7 @@ async def help(client, message):
   reply_markup = InlineKeyboardMarkup(buttons)
   await Tgraph.send_message(
         chat_id=message.chat.id,
-        text="""There Is Nothung To Know More,
+        text="""There Is Nothing To Know More,
         
 Just Send Me A Video/gif/photo Upto 5mb.
 
